18583408b855177a4ac726ac89e2b5f6d2659a87
fellypemartins <fellypemartins.official@gmail.com>
2024-09-10 02:00:29 -0300

    add PageController file

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 const express        = require('express')
 const route          = express.Router()
 const cors           = require('cors')
-const linkController = require('./controllers/LinkController')
+const linkController = require('./controllers/LinkController')  
+const pageController = require('./controllers/PageController')
 
 route.options('*', cors())
 
-// Endpoints
+// Endpoints 
+route.get('/:path', pageController.link)
 route.get('/:path', linkController.findLinkByPath) // READ
 route.delete('/:path', linkController.deleteLink) // DELETE
 route.post('/', linkController.createLink) // CREATE
