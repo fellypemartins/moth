a1d26dfb963b5e84ee895ead27020e49ece29f3d
fellypemartins <fellypemartins.official@gmail.com>
2024-09-10 22:06:21 -0300

    deleted PageController file

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,11 @@
 const express        = require('express')
 const route          = express.Router()
 const cors           = require('cors')
-const linkController = require('./controllers/LinkController')  
-const pageController = require('./controllers/PageController')
+const linkController = require('./controllers/LinkController')   
 
 route.options('*', cors())
 
-// Endpoints 
-route.get('/:path', pageController.link)
+// Endpoints  
 route.get('/:path', linkController.findLinkByPath) // READ
 route.delete('/:path', linkController.deleteLink) // DELETE
 route.post('/', linkController.createLink) // CREATE
